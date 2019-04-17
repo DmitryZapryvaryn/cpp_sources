aa67e5484d94e57fb828dfaf09f43544e30feca4
DESKTOP-IKD5KEC\Dmitry <zapdmitry@gmail.com>
2019-04-17 12:53:37 +0300

    Week 4. Refactor person cpp, remove duplicates, add PrintInfo()

--- Coursera.YellowBelt/OOP/Professions/person.cpp
+++ Coursera.YellowBelt/OOP/Professions/person.cpp
@@ -1,172 +1,86 @@
-//#include <iostream>
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
-//class Person {
-//public:
-//	Person(const string& new_name, const string& new_type) : name(new_name), type(new_type) {}
-//
-//	string GetName() const {
-//		return name;
-//	}
-//
-//	string GetType() const {
-//		return type;
-//	}
-//
-//	string GetInfo() const {
-//		return type + ": " + name;
-//	}
-//
-//	virtual void Walk(const string& destination) const {
-//		cout << GetInfo() << " walks to: " << destination << endl;
-//	}
-//
-//protected:
-//	const string type;
-//	const string name;
-//};
-//
-//class Student : public Person {
-//public:
-//
-//	Student(const string& name, const string& favouriteSong_) 
-//		: Person(name, "Student"), favouriteSong(favouriteSong_) {
-//	}
-//
-//	void Learn() const {
-//		cout << GetInfo() << " learns" << endl;
-//	}
-//
-//	void Walk(const string& destination) const {
-//		cout << GetInfo() << " walks to: " << destination << endl;
-//		SingSong();
-//	}
-//
-//	void SingSong() const {
-//		cout << GetInfo() << " sings a song: " << favouriteSong << endl;
-//	}
-//
-//private:
-//	const string favouriteSong;
-//};
-//
-//
-//class Teacher : public Person {
-//public:
-//
-//	Teacher(const string& name, const string& subject_) : Person(name, "Teacher"), subject(subject_) {}
-//
-//	void Teach() const {
-//		cout << GetInfo() << " teaches: " << subject << endl;
-//	}
-//
-//private:
-//	const string subject;
-//};
-//
-//
-//class Policeman : public Person {
-//public:
-//	Policeman(const string& name) : Person(name, "Policeman") {}
-//
-//	void Check(const Person& p) const {
-//		cout << "Policeman: " << name << " checks " << p.GetType() << ". " << p.GetType() << "'s name is: " << p.GetName() << endl;
-//	}
-//};
-//
-//
-//void VisitPlaces(const Person& base, const vector<string>& places) {
-//	for (auto p : places) {
-//		base.Walk(p);
-//	}
-//}
-//
-//int main() {
-//	Teacher t("Jim", "Math");
-//	Student s("Ann", "We will rock you");
-//	Policeman p("Bob");
-//
-//	VisitPlaces(t, { "Moscow", "London" });
-//	p.Check(s);
-//	VisitPlaces(s, { "Moscow", "London" });
-//	return 0;
-//}
-
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-class Human {
+class Person {
 public:
-	Human(const string &name, const string &job) : Name(name), Job(job) {};
+	Person(const string& new_name, const string& new_type) : name(new_name), type(new_type) {}
 
-	virtual void Walk(const string& destination) {
-		cout << Job << ": " << Name << " walks to: " << destination << endl;
+	string GetName() const {
+		return name;
 	}
 
-public:
-	const string Name;
-	const string Job;
+	string GetType() const {
+		return type;
+	}
+
+	ostream& PrintInfo() const {
+		return cout << type << ": " << name;
+	}
+
+	virtual void Walk(const string& destination) const {
+		PrintInfo() << " walks to: " << destination << endl;
+	}
+
+protected:
+	const string type;
+	const string name;
 };
 
-class Student : public Human {
+class Student : public Person {
 public:
 
-	Student(const string &name, const string &song) : Human(name, "Student"), FavouriteSong(song) {};
-
-	void Learn() {
-		cout << Job << ": " << Name << " learns" << endl;
+	Student(const string& name, const string& favouriteSong_) 
+		: Person(name, "Student"), favouriteSong(favouriteSong_) {
 	}
 
-
-	void SingSong() {
-		cout << Job << ": " << Name << " sings a song: " << FavouriteSong << endl;
+	void Learn() const {
+		PrintInfo() << " learns" << endl;
 	}
 
-	void Walk(const string& destination) override {
-		Human::Walk(destination);
+	void Walk(const string& destination) const override {
+		Person::Walk(destination);
 		SingSong();
 	}
 
-public:
-	const string FavouriteSong;
-};
-
-
-class Teacher : public Human {
-public:
-
-	Teacher(const string& name, const string& subject) : Human(name, "Teacher"), Subject(subject) {};
-
-	void Teach() {
-		cout << Job << ": " << Name << " teaches: " << Subject << endl;
+	void SingSong() const {
+		PrintInfo() << " sings a song: " << favouriteSong << endl;
 	}
 
-
-public:
-
-	const string Subject;
+private:
+	const string favouriteSong;
 };
 
 
-class Policeman : public Human {
+class Teacher : public Person {
 public:
-	Policeman(string name) : Human(name, "Policeman") {};
 
-	void Check(Human h) {
-		cout << "Policeman: " << Name << " checks " << h.Job << ". " << h.Job << "'s name is: " << h.Name << endl;
+	Teacher(const string& name, const string& subject_) : Person(name, "Teacher"), subject(subject_) {}
+
+	void Teach() const {
+		PrintInfo() << " teaches: " << subject << endl;
+	}
+
+private:
+	const string subject;
+};
+
+
+class Policeman : public Person {
+public:
+	Policeman(const string& name) : Person(name, "Policeman") {}
+
+	void Check(const Person& p) const {
+		PrintInfo() << " checks " << p.GetType() << ". " << p.GetType() << "'s name is: " << p.GetName() << endl;
 	}
 };
 
 
-void VisitPlaces(Human& h, const vector<string>& places) {
-	for (auto p : places) {
-		h.Walk(p);
+void VisitPlaces(const Person& person, const vector<string>& places) {
+	for (const auto& place : places) {
+		person.Walk(place);
 	}
 }
 
@@ -174,6 +88,7 @@ int main() {
 	Teacher t("Jim", "Math");
 	Student s("Ann", "We will rock you");
 	Policeman p("Bob");
+
 	VisitPlaces(t, { "Moscow", "London" });
 	p.Check(s);
 	VisitPlaces(s, { "Moscow", "London" });
