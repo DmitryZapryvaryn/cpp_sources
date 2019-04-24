09fd5651391050484c32f1a77eb25e2b85e29d56
DESKTOP-IKD5KEC\Dmitry <zapdmitry@gmail.com>
2019-04-24 06:15:07 +0300

    Complete profiler section

---
+++ Coursera.RedBelt/profiler.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+
+class LogDuration {
+public:
+	explicit LogDuration(const string& msg = "") : message(msg + ": "), start(steady_clock::now()) {}
+
+	~LogDuration() {
+		auto finish = steady_clock::now();
+		auto duration = finish - start;
+		cerr << message << duration_cast<microseconds>(duration).count() << " micros" << endl;
+	}
+
+private:
+	string message;
+	steady_clock::time_point start;
+};
+
+#define CONCAT_IMPL(a, b) a##b
+#define UNIQ_ID(lineno) CONCAT_IMPL(_local_uniq_var_, lineno)
+
+#define LOG_DURATION(msg) \
+	LogDuration UNIQ_ID(__LINE__) {msg};
