208b9e0182f8ece0692d5a87c26ab31c5367a241
DESKTOP-IKD5KEC\Dmitry <zapdmitry@gmail.com>
2019-07-16 01:31:48 +0300

    Format docs(ctrl+k, ctrl+d)

--- Coursera.BrownBelt/Week 1. Non-linearContainers/HashTable/hash_set.cpp
+++ Coursera.BrownBelt/Week 1. Non-linearContainers/HashTable/hash_set.cpp
@@ -9,12 +9,12 @@ using namespace std;
 template <typename Type, typename Hasher>
 class HashSet {
 public:
-  using BucketList = forward_list<Type>;
+	using BucketList = forward_list<Type>;
 
 public:
 	explicit HashSet(
 		size_t num_buckets,
-		const Hasher& hasher = {} ) : table(num_buckets), hash_function(hasher)
+		const Hasher& hasher = {}) : table(num_buckets), hash_function(hasher)
 	{}
 
 	void Add(const Type& value) {
@@ -48,91 +48,91 @@ private:
 };
 
 struct IntHasher {
-  size_t operator()(int value) const {
-    // Это реальная хеш-функция из libc++, libstdc++.
-    // Чтобы она работала хорошо, std::unordered_map
-    // использует простые числа для числа бакетов
-    return value;
-  }
+	size_t operator()(int value) const {
+		// Это реальная хеш-функция из libc++, libstdc++.
+		// Чтобы она работала хорошо, std::unordered_map
+		// использует простые числа для числа бакетов
+		return value;
+	}
 };
 
 struct TestValue {
-  int value;
+	int value;
 
-  bool operator==(TestValue other) const {
-    return value / 2 == other.value / 2;
-  }
+	bool operator==(TestValue other) const {
+		return value / 2 == other.value / 2;
+	}
 };
 
 struct TestValueHasher {
-  size_t operator()(TestValue value) const {
-    return value.value / 2;
-  }
+	size_t operator()(TestValue value) const {
+		return value.value / 2;
+	}
 };
 
 void TestSmoke() {
-  HashSet<int, IntHasher> hash_set(2);
-  hash_set.Add(3);
-  hash_set.Add(4);
+	HashSet<int, IntHasher> hash_set(2);
+	hash_set.Add(3);
+	hash_set.Add(4);
 
-  ASSERT(hash_set.Has(3));
-  ASSERT(hash_set.Has(4));
-  ASSERT(!hash_set.Has(5));
+	ASSERT(hash_set.Has(3));
+	ASSERT(hash_set.Has(4));
+	ASSERT(!hash_set.Has(5));
 
-  hash_set.Erase(3);
+	hash_set.Erase(3);
 
-  ASSERT(!hash_set.Has(3));
-  ASSERT(hash_set.Has(4));
-  ASSERT(!hash_set.Has(5));
+	ASSERT(!hash_set.Has(3));
+	ASSERT(hash_set.Has(4));
+	ASSERT(!hash_set.Has(5));
 
-  hash_set.Add(3);
-  hash_set.Add(5);
+	hash_set.Add(3);
+	hash_set.Add(5);
 
-  ASSERT(hash_set.Has(3));
-  ASSERT(hash_set.Has(4));
-  ASSERT(hash_set.Has(5));
+	ASSERT(hash_set.Has(3));
+	ASSERT(hash_set.Has(4));
+	ASSERT(hash_set.Has(5));
 }
 
 void TestEmpty() {
-  HashSet<int, IntHasher> hash_set(10);
-  for (int value = 0; value < 10000; ++value) {
-    ASSERT(!hash_set.Has(value));
-  }
+	HashSet<int, IntHasher> hash_set(10);
+	for (int value = 0; value < 10000; ++value) {
+		ASSERT(!hash_set.Has(value));
+	}
 }
 
 void TestIdempotency() {
-  HashSet<int, IntHasher> hash_set(10);
-  hash_set.Add(5);
-  ASSERT(hash_set.Has(5));
-  hash_set.Add(5);
-  ASSERT(hash_set.Has(5));
-  hash_set.Erase(5);
-  ASSERT(!hash_set.Has(5));
-  hash_set.Erase(5);
-  ASSERT(!hash_set.Has(5));
+	HashSet<int, IntHasher> hash_set(10);
+	hash_set.Add(5);
+	ASSERT(hash_set.Has(5));
+	hash_set.Add(5);
+	ASSERT(hash_set.Has(5));
+	hash_set.Erase(5);
+	ASSERT(!hash_set.Has(5));
+	hash_set.Erase(5);
+	ASSERT(!hash_set.Has(5));
 }
 
 void TestEquivalence() {
-  HashSet<TestValue, TestValueHasher> hash_set(10);
-  hash_set.Add(TestValue{2});
-  hash_set.Add(TestValue{3});
+	HashSet<TestValue, TestValueHasher> hash_set(10);
+	hash_set.Add(TestValue{ 2 });
+	hash_set.Add(TestValue{ 3 });
 
-  ASSERT(hash_set.Has(TestValue{2}));
-  ASSERT(hash_set.Has(TestValue{3}));
+	ASSERT(hash_set.Has(TestValue{ 2 }));
+	ASSERT(hash_set.Has(TestValue{ 3 }));
 
-  const auto& bucket = hash_set.GetBucket(TestValue{2});
-  const auto& three_bucket = hash_set.GetBucket(TestValue{3});
-  ASSERT_EQUAL(&bucket, &three_bucket);
+	const auto& bucket = hash_set.GetBucket(TestValue{ 2 });
+	const auto& three_bucket = hash_set.GetBucket(TestValue{ 3 });
+	ASSERT_EQUAL(&bucket, &three_bucket);
 
-  ASSERT_EQUAL(1, distance(begin(bucket), end(bucket)));
-  ASSERT_EQUAL(2, bucket.front().value);
+	ASSERT_EQUAL(1, distance(begin(bucket), end(bucket)));
+	ASSERT_EQUAL(2, bucket.front().value);
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestSmoke);
-  RUN_TEST(tr, TestEmpty);
-  RUN_TEST(tr, TestIdempotency);
-  RUN_TEST(tr, TestEquivalence);
-  return 0;
+	TestRunner tr;
+	RUN_TEST(tr, TestSmoke);
+	RUN_TEST(tr, TestEmpty);
+	RUN_TEST(tr, TestIdempotency);
+	RUN_TEST(tr, TestEquivalence);
+	return 0;
 }
