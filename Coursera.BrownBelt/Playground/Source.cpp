8b5d44751c1cea94cea1ede9626fb790ccdee68c
zapdmitry <zapdmitry@gmail.com>
2019-08-19 02:06:07 +0300

    Set up 17 standard

--- Coursera.BrownBelt/Playground/Source.cpp
+++ Coursera.BrownBelt/Playground/Source.cpp
@@ -1,21 +1,64 @@
-#include <map>
-#include <unordered_map>
-#include <string>
 #include <iostream>
+#include <memory>
 
 
 using namespace std;
 
+class Child1;
+class Child2;
+
+struct Parent {
+	virtual ~Parent() = default;
+
+	virtual void foo(const Parent& that) const = 0;
+	virtual void fooWith(const Child1& that) const = 0;
+	virtual void fooWith(const Child2& that) const = 0;
+};
+
+struct Child1 : public Parent {
+	void foo(const Parent& that) const {
+		cout << "Child1::foo" << endl;
+		that.fooWith(*this);
+	};
+	void fooWith(const Child1& that) const {
+		cout << "Child1::fooWith(Child1)" << endl;
+	};
+	void fooWith(const Child2& that) const {
+		cout << "Child1::fooWith(Child2)" << endl;
+	};
+};
+
+struct Child2 : public Parent {
+	void foo(const Parent& that) const {
+		cout << "Child2::foo" << endl;
+		that.fooWith(*this);
+	};
+	void fooWith(const Child1& that) const {
+		cout << "Child2::fooWith(Child1)" << endl;
+	};
+	void fooWith(const Child2& that) const {
+		cout << "Child2::fooWith(Child2)" << endl;
+	};
+};
+
+void bar(const Parent& first, const Parent& second) {
+	second.foo(first);
+}
+
 int main() {
-	multimap<string, int> mm;
-	mm.insert({ "AAA", 1 });
-	mm.insert({ "BBB", 2 });
-	mm.insert({ "AAA", 3 });
+	Parent* ch11 = &Child1();
+	Parent* ch21 = &Child2();
+	Parent* ch12 = &Child1();
+	Parent* ch22 = &Child2();
 
-	for (auto it = mm.lower_bound("AAA");
-		it != mm.upper_bound("AAA");
-		++it) {
-		cout << it->second << endl;
-	}
+	cout << "Child 1 - Child 1: " << endl;
+	bar(*ch11, *ch12);
+	cout << "Child 1 - Child 2: " << endl;
+	bar(*ch11, *ch21);
+	cout << "Child 2 - Child 1: " << endl;
+	bar(*ch21, *ch11);
+	cout << "Child 2 - Child 2: " << endl;
+	bar(*ch21, *ch22);
 
+	return 0;
 }
